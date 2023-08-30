77b18c5289e27c37111f4dfa6f7fe8d1ecc47d51
Camila Martinez <camilamarb98@gmail.com>
2023-08-30 16:05:26 +0200

    new nvim settings

--- lua/cammarb/plugins_config/init.lua
+++
@@ -1,5 +0,0 @@
-require("cammarb.plugins_config.lsp")
-require("cammarb.plugins_config.mason")
-require("cammarb.plugins_config.nvim-tree")
-require("cammarb.plugins_config.telescope")
-require("cammarb.plugins_config.treesitter")
