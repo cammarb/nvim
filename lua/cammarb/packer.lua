29a8d2f4227814ded3d05e5fd497158aa4c7502b
Camila Martinez <camilamarb98@gmail.com>
2023-09-10 12:05:21 +0200

    update packer

--- lua/cammarb/packer.lua
+++ lua/cammarb/packer.lua
@@ -12,4 +12,6 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use { 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
+  use('ThePrimeagen/harpoon')
+  use("tpope/vim-fugitive")
 end)
