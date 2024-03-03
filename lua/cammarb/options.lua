862c4af079aa80b18439983aaa7f2f1c34e98a4a
Camila Martinez <camilamarb98@gmail.com>
2024-03-03 14:20:41 +0100

    Update options

--- lua/cammarb/options.lua
+++ lua/cammarb/options.lua
@@ -2,9 +2,9 @@ vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.colorcolumn = "80"
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2	
+vim.opt.shiftwidth = 2
 
-
+vim.g.autoformat = true
 
 vim.g["prettier#autoformat"] = 1
 vim.g["prettier#autoformat_require_pragma"] = 0
