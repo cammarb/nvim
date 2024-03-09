38fea1c762ff0aa42329db410b4458465826456b
Camila Martinez <camilamarb98@gmail.com>
2024-03-09 17:36:43 +0100

    Update colorscheme - Remove Github theme - Add + configure Solarized Dark

--- lua/cammarb/plugins/colorscheme.lua
+++ lua/cammarb/plugins/colorscheme.lua
@@ -1,16 +1,34 @@
 return {
-	{
-		'projekt0n/github-nvim-theme',
-		lazy = false,  -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require('github-theme').setup({
-				options = {
-					--					transparent = true
-				}
-			})
+	"Tsuzat/NeoSolarized.nvim",
+	lazy = false,   -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		local ok_status, NeoSolarized = pcall(require, "NeoSolarized")
 
-			vim.cmd('colorscheme github_dark_default')
-		end,
-	}
+		if not ok_status then
+			return
+		end
+		NeoSolarized.setup {
+			style = "dark",      -- "dark" or "light"
+			transparent = false, -- true/false; Enable this to disable setting the background color
+			terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+			enable_italics = true, -- Italics for different hightlight groups (eg. Statement, Condition, Comment, Include, etc.)
+			styles = {
+				-- Style to be applied to different syntax groups
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = { bold = true },
+				variables = {},
+				string = { italic = true },
+				underline = true, -- true/false; for global underline
+				undercurl = true, -- true/false; for global undercurl
+			},
+			-- Add specific hightlight groups
+			on_highlights = function(highlights, colors)
+				-- highlights.Include.fg = colors.red -- Using `red` foreground for Includes
+			end,
+		}
+		vim.cmd [[ colorscheme NeoSolarized ]]
+	end
+
 }
