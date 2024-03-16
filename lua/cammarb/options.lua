394dddc0e91c3a9b941a8abf6272b36e546e38ab
Camila Martinez <camilamarb98@gmail.com>
2024-03-16 17:08:22 +0100

    Update colorscheme + options

--- lua/cammarb/options.lua
+++ lua/cammarb/options.lua
@@ -1,13 +1,22 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.smartindent = true
-vim.opt.colorcolumn = "80"
+
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
 
 vim.g.autoformat = true
+
+vim.opt.swapfile = false
+
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
 
 vim.g["prettier#autoformat"] = 1
 vim.g["prettier#autoformat_require_pragma"] = 0
 vim.g["prettier#config#max_line_length"] = 80
+vim.g["prettier#config#semi"] = false
 vim.g["prettier#config#tab_width"] = 2
