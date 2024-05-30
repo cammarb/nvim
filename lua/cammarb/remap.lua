84dddb1116a9e19cc6cbb6fd40d2472a5c036320
Camila Martinez <camilamarb98@gmail.com>
2024-05-30 14:18:08 +0200

    Add remap to see errors

--- lua/cammarb/remap.lua
+++ lua/cammarb/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float(0, {scope='line'})<CR>")
