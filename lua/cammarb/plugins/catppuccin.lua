8a28ff3505fe694d18f66c0465b0f0d5c7c665d6
Camila Martinez <camilamarb98@gmail.com>
2024-07-03 08:53:28 +0200

    Update

--- lua/cammarb/plugins/catppuccin.lua
+++ lua/cammarb/plugins/catppuccin.lua
@@ -4,13 +4,13 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
-      transparent_background = false,
-      flavour = "auto",
-      background = { -- :h background
-        light = "latte",
-        dark = "mocha",
-      },
+      --      transparent_background = false,
+      --      flavour = "auto",
+      --      background = { -- :h background
+      --        light = "latte",
+      --        dark = "mocha",
+      --      },
     })
-    vim.cmd('colorscheme catppuccin')
+    vim.cmd('colorscheme catppuccin-mocha')
   end
 }
