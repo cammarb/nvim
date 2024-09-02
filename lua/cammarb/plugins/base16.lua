155031aebf1dc526b6b0d8000b7c0deb4cf42951
Camila Martinez <camilamarb98@gmail.com>
2024-09-02 22:15:44 +0200

    Update

--- lua/cammarb/plugins/base16.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "RRethy/base16-nvim",
-  name = "base16-colorscheme",
-  config = function()
-    require("base16-colorscheme").with_config({})
-    --    vim.cmd("colorscheme base16-default-dark")
-  end
-}
--- lua/cammarb/remap.lua
+++ lua/cammarb/remap.lua
@@ -1,4 +1,4 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+vim.keymap.set("n", "<leader> E", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float(0, {scope='line'})<CR>")
