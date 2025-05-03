8dc27b90a81c9a7d940244b5013b79c7a2e4c5e5
Camila Martinez <camilamarb98@gmail.com>
2025-05-03 12:03:54 +0200

    feat: latex - Add and configure vimtex for live-editing .tex - Add latex to treesitter for syntax highlighting

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	build = ":TSUpdate",
 	config = function()
 	    require'nvim-treesitter.configs'.setup {
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typescript", "python", "javascript", "yaml" },
+		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typescript", "python", "javascript", "yaml", "latex" },
 		sync_install = false,
 		auto_install = true,
 		highlight = {
