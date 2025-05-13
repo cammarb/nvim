e187e74ac94f90789bd0941d1ebf6fee5905f7b5
Camila Martinez <camilamarb98@gmail.com>
2025-05-13 13:21:42 +0200

    feat(cursor): recover terminal cursor after exiting neovim

--- init.lua
+++ init.lua
@@ -10,3 +10,16 @@ set.relativenumber = true
 vim.opt.termguicolors = true
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+vim.api.nvim_create_autocmd({"VimEnter", "VimResume"}, {
+  callback = function()
+    vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon0"
+  end
+})
+
+vim.api.nvim_create_autocmd({"VimLeave", "VimSuspend"}, {
+  callback = function()
+    vim.opt.guicursor = "a:ver25-blinkon250"
+  end
+})
+
