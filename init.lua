106506e7c851d3cf9a03e73f5b0803c2e8be28f1
Camila Martinez <camila.martinez@axelspringer.com>
2025-07-29 08:43:43 +0200

    chore

--- init.lua
+++ init.lua
@@ -1,28 +1,29 @@
 require("config.lazy")
 
 local global = vim.g
-local set = vim.o
+local opt = vim.opt
 local keymap = vim.keymap
+local api = vim.api
 
 global.mapleader = " "
 
-set.shiftwidth = 2
-set.expandtab = true
-set.number = true
-set.relativenumber = true
-set.termguicolors = true
-set.swapfile = false
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.number = true
+opt.relativenumber = true
+opt.termguicolors = true
+opt.swapfile = false
 
-vim.api.nvim_create_autocmd({"VimEnter", "VimResume"}, {
+api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
   callback = function()
-    vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon0"
-  end
+    opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon0"
+  end,
 })
 
-vim.api.nvim_create_autocmd({"VimLeave", "VimSuspend"}, {
+api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
   callback = function()
-    vim.opt.guicursor = "a:ver25-blinkon250"
-  end
+    opt.guicursor = "a:ver25-blinkon250"
+  end,
 })
 
 keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
