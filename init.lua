6f9bc492b8a504ba800a98f4450f6c19765d5728
Camila Martinez <camilamarb98@gmail.com>
2025-08-03 10:07:53 +0200

    update

--- init.lua
+++ init.lua
@@ -33,10 +33,17 @@ keymap.set("n", "<leader>q", ":quit<CR>")
 
 require("config.lazy")
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 -- Autocompletion and linting
 api.nvim_create_autocmd('LspAttach', {
   group = api.nvim_create_augroup('my.lsp', {}),
+=======
+
+-- Autocompletion and linting
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+>>>>>>> d566810 (add autoformat and linting)
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method('textDocument/implementation') then
@@ -47,21 +54,36 @@ api.nvim_create_autocmd('LspAttach', {
       -- Optional: trigger autocompletion on EVERY keypress. May be slow!
       -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
       -- client.server_capabilities.completionProvider.triggerCharacters = chars
+<<<<<<< HEAD
       lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+=======
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+>>>>>>> d566810 (add autoformat and linting)
     end
     -- Auto-format ("lint") on save.
     -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
     if not client:supports_method('textDocument/willSaveWaitUntil')
         and client:supports_method('textDocument/formatting') then
+<<<<<<< HEAD
       api.nvim_create_autocmd('BufWritePre', {
         group = api.nvim_create_augroup('my.lsp', { clear = false }),
         buffer = args.buf,
         callback = function()
           lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+=======
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+>>>>>>> d566810 (add autoformat and linting)
         end,
       })
     end
   end,
 })
+<<<<<<< HEAD
 =======
 >>>>>>> 8a100b1 (update)
+=======
+>>>>>>> d566810 (add autoformat and linting)
