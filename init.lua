873aff1d66d0bd7e746742dc2c488836ff435f16
Camila Martinez <camilamarb98@gmail.com>
2025-08-06 21:03:54 +0200

    update: native lsp auto completion and linting

--- init.lua
+++ init.lua
@@ -32,7 +32,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(args)
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
     if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+    if not client:supports_method('textDocument/willSaveWaitUntil')
+        and client:supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+        buffer = args.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
     end
   end,
 })
@@ -41,4 +51,3 @@ vim.lsp.enable('lua_ls')
 vim.lsp.enable('kotlin_lsp')
 vim.lsp.enable('yamlls')
 vim.lsp.enable('gh_actions_ls')
-
