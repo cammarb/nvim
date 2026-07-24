ff43bf67aba9e65358cdab0bce1f6558526c04e5
Camila Martinez <44549364+cammarb@users.noreply.github.com>
2026-07-24 22:07:53 +0200

    fix: add proper kotlin-lsp installer
    prefer homebrew instead of manual installer

--- lsp-installer.sh
+++ lsp-installer.sh
@@ -10,19 +10,10 @@ npm i -g \
 
 # homebrew
 # https://brew.sh/
+brew install --cask kotlin-lsp
 brew install lua-language-server \
   yaml-language-server \
 
 # go
 go install github.com/docker/docker-language-server/cmd/docker-language-server@latest
-
-# Custom
-DOWNLOADS=$HOME/Downloads
-# kotlin-lsp
-# Check https://github.com/Kotlin/kotlin-lsp/releases for the latest version
-KOTLIN_LSP_DIR=/opt/kotlin-lsp
-curl -LO --output-dir "$DOWNLOADS" https://download-cdn.jetbrains.com/kotlin-lsp/262.4739.0/kotlin-server-262.4739.0.tar.gz
-sudo tar -C "$KOTLIN_LSP_DIR" -xzf "$DOWNLOADS/kotlin-server-262.4739.0.tar.gz"
-chmod +x "$KOTLIN_LSP_DIR/bin/kotlin-lsp.sh"
-sudo ln -s "$KOTLIN_LSP_DIR/bin/kotlin-lsp.sh" "$HOME/.local/bin/kotlin-lsp"
 
